0b66f375e524d683b342cb7230be4234bd22f591
Miracle-omosede <miracleigbinoghene@gmail.com>
2025-01-08 06:29:34 +0100

    fixed the signup link

--- src/pages/Competiton.jsx
+++ src/pages/Competiton.jsx
@@ -42,16 +42,16 @@ const Competiton = () => {
           {/* Text Section */}
           <div className="flex flex-col justify-center p-6 space-y-4 md:w-1/2 font-dm-sans">
             <h1 className="text-white text-2xl md:text-3xl font-bold">
-              1st EDITION - Online Writing Contest
+              1st EDITION 
             </h1>
             <p className="text-gray-300 text-sm md:text-base">
-              Participate in the inaugural online writing contest and showcase your creativity to a global audience. Amazing prizes await!
+              Participate in the inaugural contest and showcase your creativity to a global audience. Amazing prizes await!
             </p>
             <div className="flex space-x-4">
               {/* Call-to-Action Buttons */}
-              <button className="bg-dark-cyan text-white px-5 py-2 rounded-lg hover:bg-teal-600 transition duration-300">
+              <a href='https://forms.gle/Y12wZ2MytdbXNZMm6' className="bg-dark-cyan text-white px-5 py-2 rounded-lg hover:bg-teal-600 transition duration-300">
                 Sign Up
-              </button>
+              </a>
               {/* <button className="bg-transparent border border-gray-500 text-gray-300 px-5 py-2 rounded-lg hover:bg-gray-700 hover:text-white transition duration-300">
                 Learn More
               </button> */}
